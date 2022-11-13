de1e96741bea714a9bc01e1c2b35a7fca00e3c7d
liipotti <tommi.jaske@aalto.fi>
2022-11-13 16:25:24 +0200

    Use StatusController

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+{
+    var services = builder.Services;
+    services.AddControllers();
+}
+
 var app = builder.Build();
+{
+    app.MapControllers();
+}
 
 app.MapGet("/", () => "Greetings from Program.cs");
 
